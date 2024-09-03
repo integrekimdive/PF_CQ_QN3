487d1ceb03fbce533d2777158629cbbc084a1506
integrekimdiv <ngaboaugustin243@gmail.com>
2024-09-04 00:07:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
 using namespace std;
-int sum_from_to(int first, int last) {
-    return (last - first + 1) * (first + last) / 2;
+int sum_from_to(int first, int last) {//my function
+    return (last - first + 1) * (first + last) / 2;//return result
 }
 
-int main() {
-    int first, last;
+int main() {//main code
+    int first, last;//declaration of var
 
     std::cout << "Enter the first integer: ";
     std::cin >> first;
